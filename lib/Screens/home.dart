70ac84665eb1a2b5e3ac0064ccc9d70471299121
Antraxmin <antraxmin@naver.com>
2023-02-08 23:55:37 +0900

    fix: 메인 페이지 UI 수정

--- lib/Screens/home.dart
+++ lib/Screens/home.dart
@@ -17,75 +17,82 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        automaticallyImplyLeading: true,
-        centerTitle: true,
-        backgroundColor: const Color(0xffFFFFFF),
-        elevation: 0,
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            margin: const EdgeInsets.all(20),
-            alignment: Alignment.centerLeft,
-            child: const Text("모임 추가하기",
-              style: TextStyle(
+        title: const Text("모임 추가하기",
+            style: TextStyle(
               color: Colors.black,
               fontSize: 22,
               fontWeight: FontWeight.bold,
               fontFamily: 'SpoqaHanSansNeo',
             )),
-          ),
-           Container(
-             color: Colors.blueGrey[200],
-             width: 300,
-             height: 400,
-             child: ListView.builder(
-               scrollDirection: Axis.vertical,
-               padding: const EdgeInsets.all(5),
-               itemCount: listcount+1,
-               itemBuilder: (BuildContext context, int index) {
-                 return Container(
-                   margin: const EdgeInsets.all(3),
-                   height: 50,
-                   color: Colors.white,
-                   alignment: Alignment.center,
-                   child: Text('$listcount')
-                 );
-               },
-             ),
-           ),
-          const SizedBox(
-              height: 30
-          ),
-          Center(
-            child: ElevatedButton.icon(
-              onPressed: () {
-               Get.to(AddList());
-              },
-              icon: const Icon(Icons.add, size: 18),
-              label: const Text(
-                '추가하기',
-                style: TextStyle(fontSize: 20),
+        automaticallyImplyLeading: true,
+        centerTitle: true,
+        backgroundColor: const Color(0xffFFFFFF),
+        elevation: 0,
+      ),
+      body: Container(
+        //decoration: BoxDecoration(border: Border.all(color: Colors.blue)),
+        color: const Color(0xffFFFFFF),
+        //color: Colors.blueGrey,
+        child: Column(
+          //mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+
+            Expanded(
+              flex: 1,
+              child: Container(
+                decoration: BoxDecoration(border: Border.all(color: Colors.blue)),
+                width: double.infinity,
+                child: ListView.builder(
+                  scrollDirection: Axis.vertical,
+                  padding: const EdgeInsets.all(10),
+                  itemCount: 6,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Card(
+                        //alignment: Alignment.center,
+                        elevation: 10,
+                        margin: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                        color: Colors.white,
+                        child: Container(
+                          height: 50,
+                          child: Text("음식점 $index"),
+                        )
+                    );
+                  },
+                ),
               ),
-              style: ElevatedButton.styleFrom(
-                  minimumSize: const Size(200, 100), backgroundColor: Colors.green),
             ),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-           Row(
-             mainAxisAlignment: MainAxisAlignment.center,
-             children: [
-               ElevatedButton(onPressed: () {}, child: Text('초기화')),
-               SizedBox(
-                 width: 43,
-               ),
-               ElevatedButton(onPressed: () {}, child: Text('정산하기'))
-             ],
-           )
-        ],
+            Container(
+              decoration: BoxDecoration(border: Border.all(color: Colors.blue)),
+              height: 50,
+              child: Center(
+                child: ElevatedButton.icon(
+                  onPressed: () { Get.to(AddList()); },
+                  //onPressed: Navigator.push(context, MaterialPageRoute(builder: (context) => AddList()));
+                  icon: const Icon(Icons.add, size: 13),
+                  label: const Text('추가하기', style: TextStyle(fontSize: 13)),
+                  style: ElevatedButton.styleFrom(
+                      minimumSize: const Size(100, 40), backgroundColor: const Color(0xff5F9DF7)),
+                ),
+              ),
+            ),
+            /*Expanded(
+              flex: 1,
+              child: Container(
+                decoration: BoxDecoration(border: Border.all(color: Colors.blue)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(onPressed: () {}, child: Text('초기화')),
+                    SizedBox(
+                      width: 43,
+                    ),
+                    ElevatedButton(onPressed: () {}, child: Text('정산하기'))
+                  ],
+                ),
+              ),
+            )*/
+          ],
+        ),
       ),
     );
   }
