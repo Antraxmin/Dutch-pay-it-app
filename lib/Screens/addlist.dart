128e249204b3a251028175b15ce376577d1554ab
Antraxmin <antraxmin@naver.com>
2023-02-09 17:34:24 +0900

    update: addlist layout

--- lib/Screens/addlist.dart
+++ lib/Screens/addlist.dart
@@ -17,14 +17,41 @@ class _AddListState extends State<AddList> {
           Expanded(
               flex: 4,
               child: Container(
+                width: double.infinity,
                 color: const Color(0xffEEEEEE),
+                child: const Center(child: Text("영수증 이미지 업로드", style: TextStyle(fontFamily: 'SpoqaHanSansNeo', fontWeight: FontWeight.bold),)),
               )
           ),
         ],
       ),
       bottomNavigationBar: BottomAppBar(
-        child: Container(
-          height: 450.0,
+        height: 450.0,
+        child: Column(
+          children: [
+            Expanded(
+              flex: 1,
+              child: Container(
+                //height: 50,
+                //margin: const EdgeInsets.all(30),
+                decoration: BoxDecoration(border: Border.all(color: Colors.blue)),
+              ),
+            ),
+            Container(
+              height: 50,
+              width: double.infinity,
+              margin: const EdgeInsets.all(30),
+              decoration: BoxDecoration(border: Border.all(color: Colors.blue)),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: const Color(0xff002B5B), // background
+                  onPrimary: Colors.white, // foreground
+                ),
+                onPressed: () {},
+                child: const Text('저장하기', style: TextStyle(
+                    fontSize: 17, fontFamily: 'SpoqaHanSansNeo')),
+              ),
+            ),
+          ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
@@ -37,6 +64,4 @@ class _AddListState extends State<AddList> {
     );
   }
 }
-          /*floatingActionButtonLocation: FloatingActionButtonLocation.endTop,
-          floatingActionButton: FloatingActionButton(...)*/
 
